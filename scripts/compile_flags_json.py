76541f8ad6587f2d22789e4bf57ac76fb4a3b140
Jonathan Lopez <jonathanglopez@gmail.com>
2022-05-25 16:06:47 -0400

    Clean up compile_flags initialization

--- scripts/compile_flags_json.py
+++ scripts/compile_flags_json.py
@@ -2,7 +2,7 @@
 import json
 import sys
 from pathlib import Path
-from typing import TypedDict
+from typing import List, TypedDict
 
 EXTS = (
     ".c",
@@ -23,29 +23,27 @@ EXTS = (
 class CompileCommand(TypedDict):
     directory: str
     file: str
-    arguments: list[str]
+    arguments: List[str]
 
 
-compile_flags: Path = Path.cwd() / (
-    sys.argv[1] if len(sys.argv) > 1 else "compile_flags.txt"
-)
+compile_flags = Path(sys.argv[1] if len(sys.argv) > 1 else "compile_flags.txt")
 compiler = sys.argv[2] if len(sys.argv) > 2 else "clang"
 with compile_flags.open("r") as f:
     flags = [line.strip() for line in f.readlines()]
 
 project_dir: Path = compile_flags.parent
 
-compile_commands: list[CompileCommand] = []
+compile_commands: List[CompileCommand] = []
 
 for item in project_dir.glob("**/*"):
-    if not item.is_file() and item.suffix not in EXTS:
+    if not (item.is_file() and item.suffix in EXTS):
         continue
 
     compile_commands.append(
         {
-            "directory": str(project_dir),
-            "file": str(item.absolute),
-            "arguments": [compiler] + flags + [str(item.absolute)],
+            "directory": str(project_dir.absolute()),
+            "file": str(item.absolute()),
+            "arguments": [compiler] + flags + [str(item.absolute())],
         }
     )
 

248027c70190f5e5b6a6de8f36aa3de9598424f9
Jonathan Lopez <jonathanglopez@gmail.com>
2022-05-25 15:45:42 -0400

    Giving variables more descriptive names

--- scripts/compile_flags_json.py
+++ scripts/compile_flags_json.py
@@ -18,26 +18,26 @@ EXTS = (
     ".hxx",
 )
 
-input = sys.argv[1] if len(sys.argv) > 1 else "compile_flags.txt"
-driver = sys.argv[2] if len(sys.argv) > 2 else "clang"
-with open(input) as f:
+compile_flags = sys.argv[1] if len(sys.argv) > 1 else "compile_flags.txt"
+compiler = sys.argv[2] if len(sys.argv) > 2 else "clang"
+with open(compile_flags) as f:
     flags = [line.strip() for line in f]
 
-dir = os.path.dirname(os.path.abspath(input))
+project_dir = os.path.dirname(os.path.abspath(compile_flags))
 
-entries = []
+compile_commands = []
 
-for root, dirs, files in os.walk(dir):
+for root, dirs, files in os.walk(project_dir):
     for file in files:
         ext = os.path.splitext(file)[1]
         if ext in EXTS:
-            entries.append(
+            compile_commands.append(
                 {
-                    "directory": dir,
-                    "file": os.path.join(dir, file),
-                    "arguments": [driver] + flags + [os.path.join(dir, file)],
+                    "directory": project_dir,
+                    "file": os.path.join(project_dir, file),
+                    "arguments": [compiler] + flags + [os.path.join(project_dir, file)],
                 }
             )
 
-with open(os.path.join(dir, "compile_commands.json"), "w") as file:
-    json.dump(entries, file, indent=2)
+with open(os.path.join(project_dir, "compile_commands.json"), "w") as file:
+    json.dump(compile_commands, file, indent=2)

4267e447d970cf85166d1b4c075695f5dcbe34e5
Jonathan Lopez <jonathanglopez@gmail.com>
2022-05-25 15:44:08 -0400

    Formatting with black

--- scripts/compile_flags_json.py
+++ scripts/compile_flags_json.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python
-import os, sys, json
+import json
+import os
+import sys
 
-EXTS = ('.c','.C','.cpp','.cc','.cxx','.m','.mm'
-        '.h','.H','.hpp','.hh','.hxx')
+EXTS = (
+    ".c",
+    ".C",
+    ".cpp",
+    ".cc",
+    ".cxx",
+    ".m",
+    ".mm" ".h",
+    ".H",
+    ".hpp",
+    ".hh",
+    ".hxx",
+)
 
-input = sys.argv[1] if len(sys.argv) > 1 else 'compile_flags.txt'
-driver = sys.argv[2] if len(sys.argv) > 2 else 'clang'
+input = sys.argv[1] if len(sys.argv) > 1 else "compile_flags.txt"
+driver = sys.argv[2] if len(sys.argv) > 2 else "clang"
 with open(input) as f:
     flags = [line.strip() for line in f]
 
@@ -17,11 +30,13 @@ for root, dirs, files in os.walk(dir):
     for file in files:
         ext = os.path.splitext(file)[1]
         if ext in EXTS:
-            entries.append({
-                'directory': dir,
-                'file': os.path.join(dir, file),
-                'arguments': [driver] + flags + [os.path.join(dir, file)]})
+            entries.append(
+                {
+                    "directory": dir,
+                    "file": os.path.join(dir, file),
+                    "arguments": [driver] + flags + [os.path.join(dir, file)],
+                }
+            )
 
-with open(os.path.join(dir, 'compile_commands.json'), 'w') as file:
+with open(os.path.join(dir, "compile_commands.json"), "w") as file:
     json.dump(entries, file, indent=2)
-
